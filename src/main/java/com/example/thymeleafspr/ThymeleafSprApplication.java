63e13d738063f573ab3677618987f9d7f85ef510
serkan-can-eyvaz <serkancaneyvaz@gmail.com>
2024-08-19 09:42:05 +0300

    first

---
+++ src/main/java/com/example/thymeleafspr/ThymeleafSprApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafspr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSprApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafSprApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thymeleafspr/ThymeleafSprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafspr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSprApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
